b2a8fa5b978a88c06113f818cf3699d676f432ca
권석민 <sukmin.kwon@nts-corp.com>
2018-06-03 14:03:48 +0900

    gotatic 2.0.0
    - dep의존성관리로 프로젝트 변경
    - 옵션을 flag로 명시할 수 있도록 함
    - 기동시 환경정보를 출력
    - HTTP기본 인증 적용
    - 로깅기능 적용

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 # gotatic
+
+## 정적파일 서비스용도의 경량 웹서버
+
+#### 왜 만들었나?
+간단하게 파일을 공유하고 싶을때 편하게 하려고..
+
+#### 실행
+```
+#windows
+gotatic-win-2.0.0.exe
+
+#mac
+#실행권한 추가시 chmod 755 gotatic-mac-2.0.0
+./gotatic-mac-2.0.0
+
+#linux
+#실행권한 추가시 chmod 755 gotatic-linux-2.0.0
+./gotatic-linux-2.0.0
+```
+
+#### 종료
+프로세스를 죽이면 된다.
+
+#### 어떤 경로가 정적파일로 서비스되나?
+gotatic 명령어를 실행한 디렉토리
+
+예 : /home/bambi/gotatic 이 실행파일(gotatic바이너리)이고
+/home/bambi/mystatic 디렉토리에서 /home/bambi/gotatic
+명령어를 실행한 경우에는 /home/bambi/mystatic 디렉토리 하위의 파일 및 디렉토리가
+서비스 대상이 된다.
+
+#### 옵션 정보
+| 옵션        | 설명           | 기본값  | 사용예 |
+| :-------------: |-------------| :-----:|-------------|
+| p | 포트번호 | 11007 | ./gotatic -p=11007 |
+| a | HTTP기본인증사용유무 | true | ./gotatic -a=true |
+| d | 디렉토리리스팅유무 | true | ./gotatic -d=true |
+
+##### HTTP 기본인증
+웹서버 기동시 콘솔에 출력되는 username과 password를 HTTP 기본 인증에 사용
+
+username과 password는 기동시마다 변경된다.
+
+#### 버전정보
+| 버전명        | 변경사항           | 배포일  |
+| :-------------: |-------------| :-----:|
+| 2.0.0 | dep의존성관리로 프로젝트 변경<br>옵션을 flag로 명시할 수 있도록 함<br>기동시 환경정보를 출력<br>HTTP기본 인증 적용<br>로깅기능 적용     | 2018.06.03 |
+| 1.0.0 | 기본기능  | 2017.12.24 |
--- main.go
+++ main.go
@@ -1,26 +1,84 @@
 package main
 
-import "log"
-import "os"
-import "net/http"
+import (
+	"flag"
+	"fmt"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	"math/rand"
+	"net"
+	"os"
+	"strconv"
+	"time"
+)
 
 func init() {
-	log.SetOutput(os.Stdout)
-	log.SetFlags(log.Ldate | log.Lmicroseconds | log.Llongfile)
+	rand.Seed(time.Now().UnixNano())
 }
 
 func main() {
-	var port = os.Args[1]
-	log.Println("port : " + port)
 
-	currentDirectory, currentDirError := os.Getwd()
-	if currentDirError != nil {
-		log.Fatalln("Current Driectory invalid")
+	port := flag.String("p", "11007", "Http listen port. default 11007")
+	basicAuth := flag.Bool("a", true, "Http basicAuth true/false. default true")
+	directoryListing := flag.Bool("d", true, "Directory listing true/false. default true")
+	flag.Parse()
+
+	ip := LocalIp()
+	workPath, _ := os.Getwd()
+
+	fmt.Println("gotatic start")
+	fmt.Println("access url : http://" + ip + ":" + *port)
+	fmt.Println("work path : " + workPath)
+	fmt.Println("directoryListing mode :" + strconv.FormatBool(*directoryListing))
+
+	e := echo.New()
+	e.HideBanner = true
+
+	e.Use(middleware.Logger())
+	if *basicAuth {
+		getUsername := RandStringRunes(20)
+		genPassword := RandStringRunes(21)
+		fmt.Println("username : " + getUsername)
+		fmt.Println("password : " + genPassword)
+		e.Use(middleware.BasicAuth(func(username, password string, c echo.Context) (bool, error) {
+			if username == getUsername && password == genPassword {
+				return true, nil
+			}
+			return false, nil
+		}))
 	}
 
-	log.Println(currentDirectory)
+	e.Use(middleware.StaticWithConfig(middleware.StaticConfig{
+		Root:   workPath,
+		Browse: *directoryListing,
+	}))
 
-	http.Handle("/", http.FileServer(http.Dir(currentDirectory)))
-	log.Println("web server start")
-	http.ListenAndServe(":"+port, nil)
+	e.Logger.Fatal(e.Start(":" + *port))
+
+}
+
+func RandStringRunes(n int) string {
+
+	letterRunes := []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
+
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
+
+func LocalIp() string {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return ""
+	}
+	for _, address := range addrs {
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() && ipnet.IP.IsGlobalUnicast() {
+			if ipnet.IP.To4() != nil {
+				return ipnet.IP.String()
+			}
+		}
+	}
+	return "127.0.0.1"
 }
